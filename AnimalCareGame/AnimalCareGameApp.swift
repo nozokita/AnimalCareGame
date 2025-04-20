8c78dcae62d1952a2d689dfbf478bd58c1813fb0
Nozomu Kitamura <nozomu_kitamura@berkeley.edu>
2025-04-20 09:09:44 +0900

    Initial commit - Xcode project setup

---
+++ AnimalCareGame/AnimalCareGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalCareGameApp.swift
+//  AnimalCareGame
+//
+//  Created by Nozomu Kitamura on 4/20/25.
+//
+
+import SwiftUI
+
+@main
+struct AnimalCareGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
